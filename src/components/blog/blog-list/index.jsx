a900a42046746199a75d27ff74dff4c2bc3ab60a
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-10-28 00:36:46 +0200

    comments removed

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -6,25 +6,34 @@ import { useEffect, useState } from "react";
 
 const BlogList = () => {
   const [posts, setPost] = useState(null);
+  const [newData, setNewData] = useState(false);
 
   const apiUrl = process.env.REACT_APP_BE_URL;
 
   const getData = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-    };
     try {
-      const response = await fetch(`${apiUrl}/posts/`, requestOptions);
+      const response = await fetch(`${apiUrl}/posts/`);
 
       if (response.ok) {
         const data = await response.json();
         console.log("----- HOME PAGE ------");
         // console.log(data);
         setPost(data);
+        setNewData(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deletePost = async (postId) => {
+    try {
+      const response = await fetch(`${apiUrl}/posts/${postId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        console.log("DELETED");
+        setNewData(true);
       }
     } catch (error) {
       console.log(error);
@@ -33,13 +42,16 @@ const BlogList = () => {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [newData]);
   return (
     <Row>
       {posts &&
         posts.map((post) => (
           <Col key={post._id} md={4} style={{ marginBottom: 50 }}>
-            <div className="d-flex justify-content-end">
+            <div
+              className="d-flex justify-content-end"
+              onClick={() => deletePost(post._id)}
+            >
               <i className="fa fa-trash" />
             </div>
             <BlogItem {...post} />
